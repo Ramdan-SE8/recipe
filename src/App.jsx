87bcbedf7f1d23fb9d680bbd4c997e72737afdec
Ramdan-SE8 <ramdan_amir@hotmail.com>
2024-10-09 21:07:38 +0800

    Updates_20241009_Ramdan

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 import Recipe from "./components/Recipe";
+import { Card } from "./components/Card";
 
 function App() {
   const [items, setItems] = useState([]);
@@ -43,6 +44,88 @@ function App() {
       ) : (
         <p>Loading recipe...</p>
       )}
+
+<div className="App">
+      <div className="col">
+        <Card
+          imgSrc="./src/assets/images/image.png"
+          imgAlt="Card Image 1"
+          title="Recipe Name"
+          description="This is the recipe description section. You can add more details about the recipe here"
+          buttonText="Learn More"
+          link="card2"
+        />
+        <Card
+          imgSrc="./src/assets/images/beefchilli.jpg"
+          imgAlt="Card Image 2"
+          title="Recipe Name"
+          description="This is the recipe description section. You can add more details about the recipe here"
+          buttonText="Learn More"
+          link="card2"
+        />
+        <Card
+          imgSrc="./src/assets/images/carrotmuffin.jpg"
+          imgAlt="Card Image 3"
+          title="Recipe Name"
+          description="This is the recipe description section. You can add more details about the recipe here"
+          buttonText="Learn More"
+          link="card2"
+        />
+      </div>
+      <div className="col">
+        <Card
+          imgSrc="./src/assets/images/djonmustardsalmon.jpg"
+          imgAlt="Card Image 1"
+          title="Recipe Name"
+          description="This is the recipe description section. You can add more details about the recipe here"
+          buttonText="Learn More"
+          link="card1"
+        />
+        <Card
+          imgSrc="./src/assets/images/eggplant.jpeg"
+          imgAlt="Card Image 3"
+          title="Recipe Name"
+          description="This is the recipe description section. You can add more details about the recipe here"
+          buttonText="Learn More"
+          link="card2"
+        />
+        <Card
+          imgSrc="./src/assets/images/garlicpotatoes.jpg"
+          imgAlt="Card Image 3"
+          title="Recipe Name"
+          description="This is the recipe description section. You can add more details about the recipe here"
+          buttonText="Learn More"
+          link="card2"
+        />
+      </div>
+      <div className="col">
+        <Card
+          imgSrc="./src/assets/images/image.png"
+          imgAlt="Card Image 1"
+          title="Recipe Name"
+          description="This is the recipe description section. You can add more details about the recipe here"
+          buttonText="Learn More"
+          link="card2"
+        />
+        <Card
+          imgSrc="./src/assets/images/beefchilli.jpg"
+          imgAlt="Card Image 2"
+          title="Recipe Name"
+          description="This is the recipe description section. You can add more details about the recipe here"
+          buttonText="Learn More"
+          link="card2"
+        />
+        <Card
+          imgSrc="./src/assets/images/carrotmuffin.jpg"
+          imgAlt="Card Image 3"
+          title="Recipe Name"
+          description="This is the recipe description section. You can add more details about the recipe here"
+          buttonText="Learn More"
+          link="card2"
+        />
+      </div>
+      
+    </div>
     </>
   );
 }
