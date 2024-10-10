fe66ce828a7e0ef35ff22b536b5c7155e6ed74fb
FaizalHH <160080945+FaizalHH@users.noreply.github.com>
2024-10-10 21:48:57 +0800

    recipe

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import Recipe from "./components/Recipe";
 import { Card } from "./components/Card";
+import Login from "./components/Login"; 
+// import { Link } from 'react-router-dom';
+
 
 function App() {
   const [items, setItems] = useState([]);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
     setItems((prevState) => [
@@ -37,97 +41,90 @@ function App() {
   }, []);
 
   console.log(items);
-  return (
-    <>
-      {items.length > 0 ? (
-        <Recipe itemId={items[0].id} items={items} />
-      ) : (
-        <p>Loading recipe...</p>
-      )}
 
-<div className="App">
-      <div className="col">
-        <Card
-          imgSrc="./src/assets/images/image.png"
-          imgAlt="Card Image 1"
-          title="Recipe Name"
-          description="This is the recipe description section. You can add more details about the recipe here"
-          buttonText="Learn More"
-          link="card2"
-        />
-        <Card
-          imgSrc="./src/assets/images/beefchilli.jpg"
-          imgAlt="Card Image 2"
-          title="Recipe Name"
-          description="This is the recipe description section. You can add more details about the recipe here"
-          buttonText="Learn More"
-          link="card2"
-        />
-        <Card
-          imgSrc="./src/assets/images/carrotmuffin.jpg"
-          imgAlt="Card Image 3"
-          title="Recipe Name"
-          description="This is the recipe description section. You can add more details about the recipe here"
-          buttonText="Learn More"
-          link="card2"
-        />
+  return (
+    <div className="App">
+      <Login onLogin={setIsLoggedIn} />
+      <h1>Yummy Recipes</h1>
+      <div className="recipe-grid">
+        <div className="col">
+          <Card
+            imgSrc="./src/assets/images/image.png"
+            imgAlt="Card Image 1"
+            title="Recipe Name"
+            description="This is the recipe description section. You can add more details about the recipe here"
+            buttonText="Learn More"
+          />
+          <Card
+            imgSrc="./src/assets/images/beefchilli.jpg"
+            imgAlt="Card Image 2"
+            title="Recipe Name"
+            description="This is the recipe description section. You can add more details about the recipe here"
+            buttonText="Learn More"
+          />
+          <Card
+            imgSrc="./src/assets/images/carrotmuffin.jpg"
+            imgAlt="Card Image 3"
+            title="Recipe Name"
+            description="This is the recipe description section. You can add more details about the recipe here"
+            buttonText="Learn More"
+          />
+        </div>
+        <div className="col">
+          <Card
+            imgSrc="./src/assets/images/djonmustardsalmon.jpg"
+            imgAlt="Card Image 1"
+            title="Recipe Name"
+            description="This is the recipe description section. You can add more details about the recipe here"
+            buttonText="Learn More"
+          />
+          <Card
+            imgSrc="./src/assets/images/eggplant.jpeg"
+            imgAlt="Card Image 3"
+            title="Recipe Name"
+            description="This is the recipe description section. You can add more details about the recipe here"
+            buttonText="Learn More"
+          />
+          <Card
+            imgSrc="./src/assets/images/garlicpotatoes.jpg"
+            imgAlt="Card Image 3"
+            title="Recipe Name"
+            description="This is the recipe description section. You can add more details about the recipe here"
+            buttonText="Learn More"
+          />
+        </div>
+        <div className="col">
+          <Card
+            imgSrc="./src/assets/images/image.png"
+            imgAlt="Card Image 1"
+            title="Recipe Name"
+            description="This is the recipe description section. You can add more details about the recipe here"
+            buttonText="Learn More"
+          />
+          <Card
+            imgSrc="./src/assets/images/beefchilli.jpg"
+            imgAlt="Card Image 2"
+            title="Recipe Name"
+            description="This is the recipe description section. You can add more details about the recipe here"
+            buttonText="Learn More"
+          />
+          <Card
+            imgSrc="./src/assets/images/carrotmuffin.jpg"
+            imgAlt="Card Image 3"
+            title="Recipe Name"
+            description="This is the recipe description section. You can add more details about the recipe here"
+            buttonText="Learn More"
+          />
+        </div>
       </div>
-      <div className="col">
-        <Card
-          imgSrc="./src/assets/images/djonmustardsalmon.jpg"
-          imgAlt="Card Image 1"
-          title="Recipe Name"
-          description="This is the recipe description section. You can add more details about the recipe here"
-          buttonText="Learn More"
-          link="card1"
-        />
-        <Card
-          imgSrc="./src/assets/images/eggplant.jpeg"
-          imgAlt="Card Image 3"
-          title="Recipe Name"
-          description="This is the recipe description section. You can add more details about the recipe here"
-          buttonText="Learn More"
-          link="card2"
-        />
-        <Card
-          imgSrc="./src/assets/images/garlicpotatoes.jpg"
-          imgAlt="Card Image 3"
-          title="Recipe Name"
-          description="This is the recipe description section. You can add more details about the recipe here"
-          buttonText="Learn More"
-          link="card2"
-        />
-      </div>
-      <div className="col">
-        <Card
-          imgSrc="./src/assets/images/image.png"
-          imgAlt="Card Image 1"
-          title="Recipe Name"
-          description="This is the recipe description section. You can add more details about the recipe here"
-          buttonText="Learn More"
-          link="card2"
-        />
-        <Card
-          imgSrc="./src/assets/images/beefchilli.jpg"
-          imgAlt="Card Image 2"
-          title="Recipe Name"
-          description="This is the recipe description section. You can add more details about the recipe here"
-          buttonText="Learn More"
-          link="card2"
-        />
-        <Card
-          imgSrc="./src/assets/images/carrotmuffin.jpg"
-          imgAlt="Card Image 3"
-          title="Recipe Name"
-          description="This is the recipe description section. You can add more details about the recipe here"
-          buttonText="Learn More"
-          link="card2"
-        />
-      </div>
-      
+      {isLoggedIn && (
+        <>
+          <h2>Add, Post, or Review Recipes</h2>
+        </>
+      )}
     </div>
-    </>
   );
 }
+
 
 export default App;
---
+++ src/components/Login.jsx
@@ -0,0 +1,54 @@
+
+
+
+import { useState } from "react";
+import "./Login.css";
+
+const Login = ({ onLogin }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Simple mock authentication
+    if (username === "user" && password === "password123") {
+      onLogin(true);
+    } else {
+      setError("Invalid username or password");
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        {error && <p style={{ color: "red" }}>{error}</p>}
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
