7547ff7c6d4db5ef46d6a95bad0aa8a4e6e67dbf
K3lly-z <keerismyname@gmail.com>
2024-10-12 10:55:10 +0800

    added RecipeContext.jsx, UserContext.jsx, Login and Logout functions, and display of loggedin username under navbar

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,14 +1,24 @@
 import NavBar from "./NavBar";
 import { Outlet } from "react-router-dom";
 import styles from "./NavBar.module.css";
+import { RecipeProvider } from "../context/RecipeContext";
+import { UserProvider } from "../context/UserContext";
+
+
 
 const Root = () => {
+
   return (
     <>
+    <UserProvider>
       <NavBar />
+
+      <RecipeProvider>
       <div className={styles.outlet}>
         <Outlet />
       </div>
+      </RecipeProvider>
+      </UserProvider>
     </>
   );
 };
